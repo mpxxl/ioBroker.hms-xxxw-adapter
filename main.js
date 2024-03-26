8ee724574954a33f810cfdf242f5bb6fe9a46e16
Mario PIVK <Mario.PIVK@akka.eu>
2024-03-26 17:55:22 +0100

    add python call

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-
+const { PythonShell } = require("python-shell");
 // Load your modules here, e.g.:
 // const fs = require("fs");
 
@@ -46,12 +46,20 @@ class HmsXxxwAdapter extends utils.Adapter {
 
 		this.subscribeStates("*");
 
+		await this.ReadDtu();
+		this.updateInterval = setInterval(
+			async () => {
+				await this.ReadDtu();
+			},
+			this.config.interval * 60 * 1000,
+		);
+
 		/*
 		For every state in the system there has to be also an object of type state
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
-		await this.setObjectNotExistsAsync("testVariable", {
+		/*await this.setObjectNotExistsAsync("testVariable", {
 			type: "state",
 			common: {
 				name: "testVariable",
@@ -62,9 +70,9 @@ class HmsXxxwAdapter extends utils.Adapter {
 			},
 			native: {},
 		});
-
+					*/
 		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
+		//this.subscribeStates("testVariable");
 		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
 		// this.subscribeStates("lights.*");
 		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
@@ -75,23 +83,38 @@ class HmsXxxwAdapter extends utils.Adapter {
 			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
 		*/
 		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+		//await this.setStateAsync("testVariable", true);
 
 		// same thing, but the value is flagged "ack"
 		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+		//await this.setStateAsync("testVariable", { val: true, ack: true });
 
 		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+		//await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
 		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
+		/*let result = await this.checkPasswordAsync("admin", "iobroker");
 		this.log.info("check user admin pw iobroker: " + result);
 
 		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		this.log.info("check group user admin group admin: " + result);*/
 	}
 
+	async ReadDtu() {
+		PythonShell.defaultOptions = { scriptPath: "hoymiles-wifi/hoymiles-wifi" };
+
+		PythonShell.runString(
+			"from hoymiles_wifi.dtu import DTU; dtu = DTU('" +
+				this.config.host +
+				"');response = await dtu.async_get_real_data();print(response);",
+			undefined,
+		).then((messages) => {
+			// results is an array consisting of messages collected during execution
+			messages.forEach((m) => {
+				this.log.info(m);
+			});
+		});
+	}
 	/**
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 * @param {() => void} callback
@@ -102,7 +125,7 @@ class HmsXxxwAdapter extends utils.Adapter {
 			// clearTimeout(timeout1);
 			// clearTimeout(timeout2);
 			// ...
-			// clearInterval(interval1);
+			this.updateInterval && clearInterval(this.updateInterval);
 
 			callback();
 		} catch (e) {
